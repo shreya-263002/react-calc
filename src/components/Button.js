67412cb8c1cab8add02e6fc08a6860a0bdc17bfa
shreya-263002 <shreya86306@gmail.com>
2024-11-05 20:48:01 -0300

    Deploy React app to gh-pages

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@
 import React from 'react'; 
 import './Button.css';
  const Button = ({ className, value, onClick }) => { return ( 
- <button style={{ fontWeight: 'bold', fontSize: '20px' }} className={`button ${className}`} onClick={() => onClick(value)}> {value}
+ <button style={{ fontWeight: 'bold', fontSize: '32px' }} className={`button ${className}`} onClick={() => onClick(value)}> {value}
   </button>
  ); 
 };
